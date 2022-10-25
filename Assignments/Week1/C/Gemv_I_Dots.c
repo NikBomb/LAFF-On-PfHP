ad050c84cc3d5e43a31a1158a14155797b06dd41
NikBomb <nicola.bombace@gmail.com>
2022-10-25 12:43:02 +0100

    Layering Matrix with Dot product by row

--- Assignments/Week1/C/Gemv_I_Dots.c
+++ Assignments/Week1/C/Gemv_I_Dots.c
@@ -2,11 +2,11 @@
 #define chi( i )  x[ (i)*incx ]         // map chi( i )  to array x
 #define psi( i )  y[ (i)*incy ]         // map psi( i )  to array y
 
-void Dots( int, const double *, int, const double *, int, double * );
+void Dots(int n, const double *x, int incx, const double * y, int incy, double* gamma);
 
-void MyGemv( int m, int n, double *A, int ldA,
+void MyGemv(int m, int n, double *A, int ldA,
            double *x, int incx, double *y, int incy )
-{
-  for ( int i=0; i<m; i++ )
-    Dots(  ,     ,     ,     ,       ,      );
+{   for (int i = 0; i < m ; ++i){   
+     Dots(n,&alpha(i,0),ldA,x,incx,&psi(i));
+    }
 }
--- Assignments/Week1/C/data/Plot_IJP.py
+++ Assignments/Week1/C/data/Plot_IJP.py
@@ -4,11 +4,18 @@ from output_IJP import data
 
 n = [int(x) for x in data[0::6]]
 time = [float(x) for x in data[3::6]]
-
+GFLOPS = [float(x) for x in data[4::6]]
 plt.plot(n, time)
 plt.xlabel("matrix dimens m=n=k")
 plt.ylabel("Time in sec.")
 
+plt.show()
+
+plt.plot(n, GFLOPS)
+plt.xlabel("matrix dimens m=n=k")
+plt.ylabel("GFLOPS")
+ax = plt.gca()
+ax.set_ylim([0, 4])
 
 plt.show()
 
